0498168c7f3d6970dfc6472eac3baa4d9c47ee29
Thomas Yang <jscnm1993@gmail.com>
2016-06-23 12:07:52 -0400

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = (function () {
+  var notPunctReg = /[^\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+
+  function makeDefault(config) {
+    config = config || {};
+    linebreak = config.linebreak ? config.linebreak : true;
+    space = config.space || 'single';
+    if (space !== 'single' && space !== 'keep' && space !== 'none') {
+      throw new Error('Invalid space value' + space + ', should be single, keep, or none');
+    }
+  }
+
+  function punctuationize(text, config) {
+    if (!(typeof text === 'string') && !(text instanceof String)) {
+      throw new Error('Input ' + text + ' is not a string or string object.');
+    }
+    return text.replace(notPunctReg, '');
+  }
+
+  return punctuationize;
+})();
---
+++ test/test.js
@@ -0,0 +1,64 @@
+var chai = require('chai');
+var punct = require('../index.js');
+var fs = require('fs');
+
+chai.should();
+chai.use(require('chai-things'));
+
+describe('punctuationize', function () {
+  it('should throw error when input is not string', function () {
+    [
+      null,
+      undefined,
+      false,
+      Boolean(false),
+      [],
+      [[]],
+      0,
+      Number(0),
+      [0],
+      true,
+      Boolean(true),
+      1,
+      Number(1),
+      [1],
+      -1,
+      Number(-1),
+      [-1],
+      Infinity,
+      -Infinity,
+      Object(),
+      NaN
+    ].forEach(function (e) {
+      punct.bind(null, e).should.throw(Error);
+    });
+  });
+
+  it('should return a string when input is a string', function () {
+    [
+      "false",
+      "",
+      String(""),
+      "0",
+      String("0"),
+      "true",
+      "1",
+      String("1"),
+      "-1",
+      String("-1"),
+    ].forEach(function (e) {
+      t = punct(e);
+      t.should.be.a('string');
+    });
+  });
+
+  it('should wipe out every ascii char but punctuations by default', function () {
+    str = Array.apply(null, new Array(127 - 32)).map(function (e, i) {
+      return String.fromCharCode(i + 32);
+    }).join('');
+    ans = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    t = punct(str);
+    t.should.be.a('string');
+    t.should.equals(ans);
+  });
+});
