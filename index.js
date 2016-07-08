679fa51fec7f2370e81e5dbc5b734ba9f92b49b8
Thomas Yang <jscnm1993@gmail.com>
2016-07-08 17:21:00 -0400

    add eslint and polish styling accordingly

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-"use strict";
-module.exports = (function () {
-  var notPunctReg = /[^\s\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+module.exports = (function Punctuationize() {
+  const notPunctReg = /[^\s\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
 
-  function makeDefault(config) {
-    config = config || {};
+  function makeDefault(configObj) {
+    const config = configObj || {};
     config.space = config.space || 'single';
     return config;
   }
 
-  function punctuationize(text, config) {
-    if (!(typeof text === "string" || text instanceof String)) {
-      throw new Error("Input " + text + " is not a string or string object.");
+  function punctuationize(text, configObj) {
+    if (!(typeof text === 'string' || text instanceof String)) {
+      throw new Error(`Input ${text} is not a string or string object.`);
     }
-    var res = text.replace(notPunctReg, '');
-    config = makeDefault(config);
+    let res = text.replace(notPunctReg, '');
+    const config = makeDefault(configObj);
     if (config.space === 'none') {
       res = res.replace(/\s/g, '');
-    }
-    else if (config.space === 'single') {
+    } else if (config.space === 'single') {
       // keep only single line break, in unix style
       res = res.replace(/\r\n/g, '\n')
         .replace(/\r/g, '\n')
@@ -30,8 +28,7 @@ module.exports = (function () {
         .replace(/\s\s+/g, ' ');
     }
     return res;
-
   }
 
   return punctuationize;
-})();
+}());
--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
-var chai = require('chai');
-var punct = require('../index.js');
+const chai = require('chai');
+const punct = require('../index.js');
 
 chai.should();
 chai.use(require('chai-things'));
 
-describe('punctuationize', function () {
-  it('should throw error when input is not string', function () {
+describe('punctuationize', () => {
+  it('should throw error when input is not string', () => {
     [
       null,
       undefined,
@@ -27,55 +27,55 @@ describe('punctuationize', function () {
       Infinity,
       -Infinity,
       {},
-      NaN
-    ].forEach(function (e) {
+      NaN,
+    ].forEach((e) => {
       punct.bind(null, e).should.throw(Error);
     });
   });
 
-  it('should return a string when input is a string', function () {
+  it('should return a string when input is a string', () => {
     [
-      "false",
-      "",
-      String(""),
-      "0",
-      String("0"),
-      "true",
-      "1",
-      String("1"),
-      "-1",
-      String("-1"),
-    ].forEach(function (e) {
+      'false',
+      '',
+      String(''),
+      '0',
+      String('0'),
+      'true',
+      '1',
+      String('1'),
+      '-1',
+      String('-1'),
+    ].forEach((e) => {
       punct(e).should.be.a('string');
     });
   });
 
-  it('should wipe out every ascii char but punctuation marks', function () {
-    var ascS = 32,
-        ascE = 127;
-    var str = Array.apply(null, new Array(ascE - ascS)).map(function (_, i) {
-      return String.fromCharCode(i + ascS);
-    }).join('');
-    var ans = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
-    var t = punct(str, {space: 'keep'});
+  it('should wipe out every ascii char but punctuation marks', () => {
+    const ascS = 32;
+    const ascE = 127;
+    const str = Array.apply(null, new Array(ascE - ascS)).map((_, i) =>
+      String.fromCharCode(i + ascS)
+    ).join('');
+    const ans = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    const t = punct(str, { space: 'keep' });
     t.should.be.a('string');
     t.should.equals(ans);
   });
 
-  it('should wipe out space if specified', function () {
-    var str = ' \f\n\r\t\v\u00a0\u1680\u180e\u2000\u2001\u2002\u2003\u2004' +
+  it('should wipe out space if specified', () => {
+    const str = ' \f\n\r\t\v\u00a0\u1680\u180e\u2000\u2001\u2002\u2003\u2004' +
               '\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f' +
               '\u3000\ufeff';
-    var ans = '';
-    var t = punct(str, {space: 'none'});
+    const ans = '';
+    const t = punct(str, { space: 'none' });
     t.should.be.a('string');
     t.should.equals(ans);
   });
 
-  it('should keep only one space for repeating space, speficied or by default', function () {
-    var str = ' ;  ;; ;     ; \n\n ; \r\n\r\n;;;  ; \r ;';
-    var ans = '; ;; ; ;\n;\n;;; ;\n;';
-    var t = punct(str, {space: 'single'});
+  it('should keep only one space for repeating space, speficied or by default', () => {
+    const str = ' ;  ;; ;     ; \n\n ; \r\n\r\n;;;  ; \r ;';
+    const ans = '; ;; ; ;\n;\n;;; ;\n;';
+    let t = punct(str, { space: 'single' });
     t.should.be.a('string');
     t.should.equals(ans);
 
@@ -85,16 +85,16 @@ describe('punctuationize', function () {
   });
 
 
-  it('should work well with unicode punctuation marks', function () {
-    var gPunctS = 0x2000,
-        gPunctE = 0x2070,
-        sPunctS = 0x2e00,
-        sPunctE = 0x2e80;
-    [[gPunctS, gPunctE], [sPunctS, sPunctE]].forEach(function (range) {
-      str = Array.apply(null, new Array(range[1] - range[0])).map(function (_, i) {
-        return String.fromCharCode(i + range[0]);
-      }).join('');
-      var t = punct(str, {space: 'keep'});
+  it('should work well with unicode punctuation marks', () => {
+    const gPunctS = 0x2000;
+    const gPunctE = 0x2070;
+    const sPunctS = 0x2e00;
+    const sPunctE = 0x2e80;
+    [[gPunctS, gPunctE], [sPunctS, sPunctE]].forEach((range) => {
+      const str = Array.apply(null, new Array(range[1] - range[0])).map((_, i) =>
+        String.fromCharCode(i + range[0])
+      ).join('');
+      const t = punct(str, { space: 'keep' });
       t.should.be.a('string');
       t.should.equals(str);
     });
