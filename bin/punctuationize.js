bc640f1b81b576a6b9fbf74c5c09defdea68ab73
YangHJ <jscnm1993@gmail.com>
2016-08-14 21:59:21 -0400

    add cli

--- bin/punctuationize.js
+++ bin/punctuationize.js
@@ -1,13 +1,33 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
 const punctuationize = require('../index.js');
 const commander = require('commander');
 
 commander
   .version('0.0.1')
-  .option('-s, --space [single]', 'specify how to handle spaces [single, none, keep]', 'single')
-  .option('-o, --output <file>', 'output file')
+  .usage('[options] <file>')
+  .option('-s, --space <single>', 'specify how to handle spaces [single, none, keep]', 'single')
+  .option('-o, --output <file>', 'specify output file, or it will be stdout')
   .parse(process.argv);
 
-console.log('output' + commander.output);
-console.log('space' + commander.space);
+if (!commander.args || commander.args.length !== 1) {
+  commander.help();
+}
+if (commander.space !== 'single' &&
+    commander.space !== 'none' &&
+    commander.space !== 'keep') {
+  commander.help();
+}
+
+let fd;
+fd = fs.readFileSync(path.join('.', commander.args[0]));
+fd = fd.toString();
+const res = punctuationize(fd, { space: commander.space });
+
+if (commander.output === undefined) {
+  process.stdout.write(res);
+} else {
+  fs.writeFileSync(path.join('.', commander.output), res);
+}
